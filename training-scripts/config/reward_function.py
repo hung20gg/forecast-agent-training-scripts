c0a7a1ad54d6c558b6d6fd9fb1d926f68f91d6ed
hung20gg <quanghung20gg@gmail.com>
2026-03-08 13:05:41 +0700

    fix: change inference params in vllm for suitable test + fix reward func nll < 0

--- training-scripts/config/reward_function.py
+++ training-scripts/config/reward_function.py
@@ -1,7 +1,9 @@
+import os
 import re
 import math
 from datetime import datetime, timedelta
 import json
+from uuid import uuid4
 
 # Extract <answer> ... </answer> from the response
 def extract_answer(response: str) -> str:
@@ -33,7 +35,7 @@ def extract_numerical_answer(response: str) -> tuple[float, float]:
         return None, None
 
 
-def chi2_pdf(x, df=15):
+def chi2_pdf(x, df=13):
     if x < 0:
         return 0.0
     
@@ -44,12 +46,20 @@ def chi2_pdf(x, df=15):
 def nll_exclude_min(pred_mean, pred_std, true_mean, true_std = 0):
 
     if true_std == 0:
-        true_std = true_mean * 0.1
+        true_std = abs(true_mean) * 0.05
 
-    return math.log(pred_std/true_std) + 0.5 * (true_std ** 2 + (pred_mean - true_mean) ** 2) / true_std ** 2 - 0.5
+    if pred_std == 0:
+        pred_std = abs(pred_mean) * 0.01
+
+    return (
+        math.log(true_std / pred_std) +
+        (pred_std**2 + (pred_mean - true_mean)**2) /
+        (2 * true_std**2) -
+        0.5
+    )
 
 
-def compute_score(data_source, solution_str, ground_truth, extra_info, alpha = 0.5):
+def compute_score(data_source, solution_str, ground_truth, extra_info, alpha = 2, beta = 2):
 
     # Extra infos:
     # {
@@ -62,22 +72,31 @@ def compute_score(data_source, solution_str, ground_truth, extra_info, alpha = 0
     #     "id": "c16c4a04-5501-491c-a127-3a9a2fe951cb"
     # }
 
-    print("### Extra info:", extra_info)
-
     max_date_time = datetime.strptime(extra_info['time_asked'], '%Y-%m-%d %H:%M:%S')
-    tool_calls = []
+    tool_calls = extra_info['tool_calls']
+
+    # If the agent does not call any tool, it is likely that the agent does not know how to solve the problem, thus we give a reward of -1 to encourage the agent to call tools in future.
+    if len(tool_calls) == 0:
+        return -1
+    
+    
+    os.makedirs('logs/answers', exist_ok=True)
+
+    with open(f"logs/answers/{extra_info['id']}.txt", 'w', encoding='utf-8') as f:
+        f.write(solution_str)
 
     # tool_calls = extract_tool_call(solution_str)
-    # for tool_call in tool_calls:
-    #     tool_call = json.loads(tool_call)
-    #     for key, value in tool_call.get('arguments').items():
-    #         if 'date' in key:
-    #             try:
-    #                 date_time = datetime.strptime(value, '%Y-%m-%d')
-    #             except ValueError:
-    #                 date_time = datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
-    #             if date_time > max_date_time + timedelta(days=1):
-    #                 return -1
+    for tool_call in tool_calls:
+        # tool_call = json.loads(tool_call)
+        for key, value in tool_call.get('arguments').items():
+            if 'date' in key:
+                try:
+                    date_time = datetime.strptime(value, '%Y-%m-%d')
+                except ValueError:
+                    date_time = datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
+                if date_time > max_date_time + timedelta(days=1):
+                    
+                    return -1
     
     # answer = extract_answer(solution_str)
     answer = solution_str.split("</think>")[-1].strip()
@@ -90,8 +109,8 @@ def compute_score(data_source, solution_str, ground_truth, extra_info, alpha = 0
     gt_mean = ground_truth['mean']
     gt_std = ground_truth['std']
 
-    print(f"### Extracted answer: mean={mean}, std={std}, ground_truth_mean={gt_mean}, ground_truth_std={gt_std}, reward={max( - alpha * nll_exclude_min(mean, std, gt_mean, gt_std) + 1, -1)}")
+    print(f"### Extracted answer: mean={mean}, std={std}, ground_truth_mean={gt_mean}, ground_truth_std={gt_std}, reward={max( - 1/beta * nll_exclude_min(mean, std, gt_mean, gt_std) + 1, -1)}")
     
-    print('### Reward:', nll_exclude_min(mean, std, gt_mean, gt_std))
+    print('### NLL Score:', nll_exclude_min(mean, std, gt_mean, gt_std))
 
-    return chi2_pdf(len(tool_calls)) + max( - alpha * nll_exclude_min(mean, std, gt_mean, gt_std) + 1, -1)
+    return alpha * chi2_pdf(len(tool_calls)) + max( - 1/beta * nll_exclude_min(mean, std, gt_mean, gt_std) + 1, -1)
