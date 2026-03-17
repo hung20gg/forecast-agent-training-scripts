c1c0e34396ca61f4e59ceaad0fbc92814f720f95
hung20gg <quanghung20gg@gmail.com>
2026-03-17 23:16:54 +0700

    hehe

--- training-scripts/run_qwen3_single_gpu.sh
+++ training-scripts/run_qwen3_single_gpu.sh
@@ -14,7 +14,7 @@ if [ "$size" == "micro" ]; then
     echo "Downloading a small subset of the dataset for quick testing..."
     python "$PROJECT_DIR/training-scripts/download.py" --local_save_dir $DATASET_DIR --limit_rows 50
     
-else if [ "$size" == "small" ]; then
+elif [ "$size" == "small" ]; then
     python "$PROJECT_DIR/training-scripts/download.py" --local_save_dir $DATASET_DIR --limit_rows 64
 
 else
